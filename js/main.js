2dc5017bbaf500995419ef3a470fdc86582dfd04
NikolayMiroshnichenko <36457433+NikolayMiroshnichenko@users.noreply.github.com>
2020-04-05 21:53:39 +0300

    9

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ const modal = document.querySelector('.js-modal-wrapper');
 const closeModalBtn = document.querySelector('.main-btn-yes');
 
 
-// setTimeout(() => {
-//     openModal();    
-// }, 3000);
+setTimeout(() => {
+    openModal();    
+}, 3000);
 
 closeModalBtn.addEventListener('click', closeModal);
 
